1695ff5587a8045b686a9f916cde73ee0b58d27e
Adriano Damas <42849035+AdrianoDamas@users.noreply.github.com>
2026-02-25 11:30:12 -0300

    Create Entity base class for Domain Model

---
+++ src/CRM.Domain/Core/Entity.cs
@@ -0,0 +1,30 @@
+namespace CRM.Domain.Core;
+
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; protected set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity other)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
